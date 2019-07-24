27fd45e696c0f8307e733f4e3b01aab911978563
BihackerViktor <bihacker.viktor@gmail.com>
2019-07-24 13:56:52 +0200

    2health

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -25,17 +25,28 @@ public class Main extends Application {
         mainScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
 
         int actualHealth = Globals.getInstance().snake.getHealth();
+        int actualHealth2 = Globals.getInstance().snake2.getHealth();
         Text t = new Text(750, 40, String.format("Health: %d", actualHealth));
+        Text t2 = new Text(50,40,String.format("Health: %d", actualHealth2));
         DropShadow ds = new DropShadow();
         ds.setOffsetY(3.0f);
         ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
-        t.setFill(Color.MEDIUMPURPLE);
+
+        t.setFill(Color.DARKRED);
         t.setFontSmoothingType(FontSmoothingType.LCD);
         t.setEffect(ds);
         t.setCache(true);
         t.setFont(Font.font(null, FontWeight.BOLD, 32));
 
+        t2.setFill(Color.RED);
+        t2.setFontSmoothingType(FontSmoothingType.LCD);
+        t2.setEffect(ds);
+        t2.setCache(true);
+        t2.setFont(Font.font(null, FontWeight.BOLD, 32));
+
         game.getChildren().add(t);
+        game.getChildren().add(t2);
+
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(mainScene);
         primaryStage.show();
