db3f06ae9830cf4ef9241c22d11a54ed26702210
BihackerViktor <bihacker.viktor@gmail.com>
2019-07-24 18:55:59 +0200

    setimage

--- src/com/codecool/snake/entities/snakes/SnakeHealth.java
+++ src/com/codecool/snake/entities/snakes/SnakeHealth.java
@@ -1,6 +1,7 @@
 package com.codecool.snake.entities.snakes;
 
 import com.codecool.snake.Globals;
+import javafx.scene.image.Image;
 
 public class SnakeHealth {
 
@@ -12,9 +13,15 @@ public class SnakeHealth {
     public void getNumberOfLives() {
         if (getActualHealth() < 1) {
         } else if (getActualHealth() >= 1 && getActualHealth() < 140) {
+
+            setImage(Globals.getInstance().getImage("zerolife"));
+
         } else if (getActualHealth() >= 140 && getActualHealth() < 180) {
         } else if (getActualHealth() >= 160 && getActualHealth() < 220) {
         } else {
         }
+    }
+
+    private void setImage(Image zerolife) {
     }
 }
