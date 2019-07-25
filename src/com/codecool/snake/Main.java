662367f41517b3c83d15047179342c2917575500
Roy91 <rolonew@gmail.com>
2019-07-25 21:04:36 +0200

    musica

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -2,7 +2,12 @@ package com.codecool.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+
+import java.io.File;
 
 
 public class Main extends Application {
@@ -13,17 +18,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
         Game game = new Game();
         Scene mainScene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
+
         mainScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(mainScene);
         primaryStage.show();
+
         game.start();
+
+        String bip = "/home/roland1/Codecool/OOP/3TW/Snake072219/intro-music.wav";
+        Media hit = new Media(new File(bip).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+        mediaPlayer.setAutoPlay(true);
     }
 
     @Override
     public void stop() throws Exception {
         System.out.println("Exiting..");
     }
+
 }
