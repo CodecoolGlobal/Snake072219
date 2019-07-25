6cf8a4149ec758f1f5d8b45db604da9bbc0bc46e
BihackerViktor <bihacker.viktor@gmail.com>
2019-07-25 10:24:45 +0200

    health complete

--- src/com/codecool/snake/entities/snakes/SnakeHealth.java
+++ src/com/codecool/snake/entities/snakes/SnakeHealth.java
@@ -1,66 +1,86 @@
 package com.codecool.snake.entities.snakes;
 
 import com.codecool.snake.Globals;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class SnakeHealth extends ImageView {
+public class SnakeHealth {
 
     public int getActualHealth() {
-        int SnakeHealth1 = Globals.getInstance().snake2.getHealth();
+        int SnakeHealth1 = Globals.getInstance().snake.getHealth();
         return SnakeHealth1;
+    }
+
+    public int getActualHealth2() {
+        int SnakeHealth2 = Globals.getInstance().snake2.getHealth();
+        return SnakeHealth2;
     }
 
     public void getNumberOfLives() {
         if (getActualHealth() < 1) {
-            System.out.println("0");
+            Image result1 = new Image("0.png");
+            ImageView result21 = new ImageView(result1);
+            result21.setX(20);
+            result21.setY(20);
+            Globals.getInstance().game.getChildren().add(result21);
         } else if (getActualHealth() >= 1 && getActualHealth() < 140) {
-
-            System.out.println("1");
-            setImage(Globals.getInstance().getImage("zerolife"));
-            setX(100);
-            setY(100);
-
+            Image result1 = new Image("1i.png");
+            ImageView result21 = new ImageView(result1);
+            result21.setX(20);
+            result21.setY(20);
+            Globals.getInstance().game.getChildren().add(result21);
         } else if (getActualHealth() >= 140 && getActualHealth() < 180) {
-
-            System.out.println("2");
-
-        } else if (getActualHealth() >= 160 && getActualHealth() < 220) {
-
-            System.out.println("3");
-
+            Image result1 = new Image("2i.png");
+            ImageView result21 = new ImageView(result1);
+            result21.setX(20);
+            result21.setY(20);
+            Globals.getInstance().game.getChildren().add(result21);
+        } else if (getActualHealth() >= 180 && getActualHealth() < 220) {
+            Image result1 = new Image("3i.png");
+            ImageView result21 = new ImageView(result1);
+            result21.setX(20);
+            result21.setY(20);
+            Globals.getInstance().game.getChildren().add(result21);
         } else {
-
-            System.out.println("4");
+            Image result1 = new Image("4i.png");
+            ImageView result21 = new ImageView(result1);
+            result21.setX(20);
+            result21.setY(20);
+            Globals.getInstance().game.getChildren().add(result21);
 
         }
     }
 
-    public int getActualHealth2() {
-        int SnakeHealth1 = Globals.getInstance().snake2.getHealth();
-        return SnakeHealth1;
-    }
-
     public void getNumberOfLives2() {
-        if (getActualHealth() < 1) {
-            System.out.println("20");
-        } else if (getActualHealth() >= 1 && getActualHealth() < 140) {
-
-            System.out.println("21");
-            setImage(Globals.getInstance().getImage("zerolife"));
-            setX(100);
-            setY(100);
-
-        } else if (getActualHealth() >= 140 && getActualHealth() < 180) {
-
-            System.out.println("22");
-
-        } else if (getActualHealth() >= 160 && getActualHealth() < 220) {
-
-            System.out.println("23");
-
+        if (getActualHealth2() < 1) {Image result = new Image("0.png");
+            ImageView result2 = new ImageView(result);
+            result2.setX(800);
+            result2.setY(20);
+            Globals.getInstance().game.getChildren().add(result2);
+        } else if (getActualHealth2() >= 1 && getActualHealth2() < 140) {
+            Image result = new Image("1.png");
+            ImageView result2 = new ImageView(result);
+            result2.setX(800);
+            result2.setY(20);
+            Globals.getInstance().game.getChildren().add(result2);
+        } else if (getActualHealth2() >= 140 && getActualHealth2() < 180) {
+            Image result = new Image("2.png");
+            ImageView result2 = new ImageView(result);
+            result2.setX(800);
+            result2.setY(20);
+            Globals.getInstance().game.getChildren().add(result2);
+        } else if (getActualHealth2() >= 180 && getActualHealth2() < 220) {
+            Image result = new Image("3.png");
+            ImageView result2 = new ImageView(result);
+            result2.setX(800);
+            result2.setY(20);
+            Globals.getInstance().game.getChildren().add(result2);
         } else {
-
-            System.out.println("24");
+            Image result = new Image("4.png");
+            ImageView result2 = new ImageView(result);
+            result2.setX(800);
+            result2.setY(20);
+            Globals.getInstance().game.getChildren().add(result2);
 
         }
     }
