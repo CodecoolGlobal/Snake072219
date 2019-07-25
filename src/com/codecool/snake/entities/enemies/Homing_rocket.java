2cdb24b9699e66a004bcc6b31a0be57dc48b0716
Roy91 <rolonew@gmail.com>
2019-07-25 13:20:49 +0200

    hotfix beambeam

--- src/com/codecool/snake/entities/enemies/Homing_rocket.java
+++ src/com/codecool/snake/entities/enemies/Homing_rocket.java
@@ -19,8 +19,8 @@ public class Homing_rocket extends Enemy implements Animatable, Interactable {
     private Point2D heading;
     private static Random rnd = new Random();
 
-    public Homing_rocket(){
-        super(-20,-10);
+    public Homing_rocket() {
+        super(-20, -10);
 
         setImage(Globals.getInstance().getImage("Enemy2"));
         setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
@@ -32,38 +32,63 @@ public class Homing_rocket extends Enemy implements Animatable, Interactable {
         int speed = 2;
         heading = Utils.directionToVector(direction, speed);
     }
+
     @Override
     public void step() {
         if (isOutOfBounds()) {
-            heading = Utils.directionToVector(getRotate()-60, 2);
-            setRotate(getRotate()-60);
+            heading = Utils.directionToVector(getRotate() - 60, 2);
+            setRotate(getRotate() - 60);
         }
-        setX(getX() + heading.getX());
-        setY(getY() + heading.getY());
-    }
 
-    @Override
-    public void apply(GameEntity entity) {
-        if(entity instanceof SnakeHead){
-            System.out.println(getMessage());
-            destroy();
-        }
-        if(entity instanceof Beam){
-            destroy();
-        }
         List<Snake> snakes = new ArrayList<>();
         snakes.add(Globals.getInstance().snake);
         snakes.add(Globals.getInstance().snake2);
         int index = rnd.nextInt(snakes.size());
         snakes.get(index).getHead();
 
-        double dir = Utils.getDirectionBetweenTwo(this, snakes.get(index).getHead());
+        double dir = Utils.getDirectionBetweenTwo(this, getClosestSnake());
         setRotate(dir);
-        int speed = 2;
-        heading = Utils.directionToVector(dir,speed);
+        double speed = 1;
+        heading = Utils.directionToVector(dir, speed);
+
         setX(getX() + heading.getX());
         setY(getY() + heading.getY());
     }
+
+    public SnakeHead getClosestSnake() {
+
+        List<Snake> snakes = new ArrayList<>();
+        snakes.add(Globals.getInstance().snake);
+        snakes.add(Globals.getInstance().snake2);
+        List<Double> distances = new ArrayList<>();
+
+        for (Snake snake : snakes) {
+            double deltaX = snake.getHead().getX() - getX();
+            double deltaY = snake.getHead().getY() - getY();
+            double distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+            distances.add(distance);
+        }
+        if (distances.get(0) < distances.get(1)) {
+            return Globals.getInstance().snake.getHead();
+        } else {
+            return Globals.getInstance().snake2.getHead();
+        }
+    }
+
+    @Override
+    public void apply(GameEntity entity) {
+        if (entity instanceof SnakeHead) {
+            System.out.println(getMessage());
+            destroy();
+        }
+
+        if (entity instanceof Beam) {
+            destroy();
+        }
+        setX(getX() + heading.getX());
+        setY(getY() + heading.getY());
+    }
+
     @Override
     public String getMessage() {
         return (getDamage() + " damage");
--- src/com/codecool/snake/entities/snakes/SnakeHead.java
+++ src/com/codecool/snake/entities/snakes/SnakeHead.java
@@ -69,7 +69,7 @@ public class SnakeHead extends GameEntity implements Interactable {
         if (entity instanceof SnakeHead) {
             try {
                 snake.gameOverPopUp();
-                Thread.sleep(4);
+                Thread.sleep(40);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
