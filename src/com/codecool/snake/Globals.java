dfd6ba71f19b8c7935433bb1391fd640f0f41910
Roy91 <rolonew@gmail.com>
2019-07-25 10:06:26 +0200

    hotfix to beam

--- src/com/codecool/snake/Globals.java
+++ src/com/codecool/snake/Globals.java
@@ -1,8 +1,12 @@
 package com.codecool.snake;
 
+import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.snakes.Snake;
 import com.codecool.snake.resources.Resources;
 import javafx.scene.image.Image;
+
+import java.util.Collections;
+import java.util.List;
 
 // class for holding all static stuff
 public class Globals {
@@ -32,13 +36,14 @@ public class Globals {
 
     public void setupResources() {
         resources = new Resources();
-        resources.addImage("SnakeHead", new Image("snake_head.png"));
-        resources.addImage("SnakeBody", new Image("snake_body.png"));
+        resources.addImage("SnakeHead", new Image("snake_headBLUE.png"));
+        resources.addImage("SnakeBody", new Image("snake_bodyBLUE.png"));
         resources.addImage("Bouncer_enemy", new Image("worm_enemy.png"));
         resources.addImage("Enemy2", new Image("enemy_created_first.png"));
         resources.addImage("PowerUpBerry", new Image("powerup_berry.png"));
         resources.addImage("PowerUpNut", new Image("powerup_nut.png"));
         resources.addImage("PowerUpSpeed", new Image("powerup_speed.png"));
+        resources.addImage("Beam", new Image("laserc.gif"));
 
     }
 
@@ -52,6 +57,12 @@ public class Globals {
 
     public void stopGame() {
         gameLoop.stop();
+    }
+
+    public static List<GameEntity> gameObjects;
+
+    public static List<GameEntity> getGameObjects() {
+        return Collections.unmodifiableList(gameObjects);
     }
 
     private Globals() {
